aac43ee75765b403341aa96c083fa65e30d20fb9
Jose Angarita <amjose09@gmail.com>
2022-05-08 18:49:36 -0500

    get de entry creado

--- 03-open-jira/src/pages/api/entries/[id].ts
+++ 03-open-jira/src/pages/api/entries/[id].ts
@@ -15,6 +15,9 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
         case "PUT":
             return updateEntry(req, res)
 
+        case "GET":
+            return getEntry(req, res)
+
         default:
             return res.status(405).json({
                 message: "Method not allowed"
@@ -57,4 +60,21 @@ export const updateEntry = async (req: NextApiRequest, res: NextApiResponse<Data
         return res.status(500).json(JSON.stringify(error) as any)
     }
 
+}
+
+export const getEntry = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
+
+    const { id } = req.query
+
+    try {
+        const entry = await prisma.entry.findUnique({
+            where: { id: id as string }
+        })
+        if (!entry) {
+            return res.status(404).json({ message: "Entry not found" + id })
+        }
+        return res.status(200).json(entry)
+    } catch (error) {
+
+    }
 }
