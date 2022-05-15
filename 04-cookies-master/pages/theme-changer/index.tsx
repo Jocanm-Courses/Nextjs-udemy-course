3c4459d223614821c8ad7d7192f379310b07c3a3
Jose Angarita <amjose09@gmail.com>
2022-05-15 17:57:52 -0500

    js-cookie instalado

--- 04-cookies-master/pages/theme-changer/index.tsx
+++ 04-cookies-master/pages/theme-changer/index.tsx
@@ -1,10 +1,32 @@
-import React from 'react'
+import { Card, CardContent, FormControl, FormControlLabel, FormLabel, Radio, RadioGroup } from '@mui/material'
+import React, { useState } from 'react'
 import { Layout } from '../../src/components/layout'
 
 const ThemeChangerPage = () => {
+
+    const [currentTheme, setCurrentTheme] = useState("light")
+
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setCurrentTheme(event.target.value)
+    }
+
     return (
         <Layout>
-            <h1>ThemeChangerPage</h1>
+            <Card>
+                <CardContent>
+                    <FormControl>
+                        <FormLabel>Tema</FormLabel>
+                        <RadioGroup
+                            value={currentTheme}
+                            onChange={handleChange}
+                        >
+                            <FormControlLabel value="light" control={<Radio />} label="Light" />
+                            <FormControlLabel value="dark" control={<Radio />} label="Dark" />
+                            <FormControlLabel value="custom" control={<Radio />} label="Custom" />
+                        </RadioGroup>
+                    </FormControl>
+                </CardContent>
+            </Card>
         </Layout>
     )
 }
