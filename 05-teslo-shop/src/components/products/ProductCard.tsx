51285bc4a792ba9e9e90efa70b8b5bb160bda7ff
Jose Angarita <amjose09@gmail.com>
2022-05-16 17:43:24 -0500

    pagina para la descripcion de un producto creada

--- 05-teslo-shop/src/components/products/ProductCard.tsx
+++ 05-teslo-shop/src/components/products/ProductCard.tsx
@@ -1,28 +1,58 @@
-import React, { FC } from 'react'
+import React, { FC, useMemo, useState } from 'react'
 import { IProduct } from '../../interfaces/products';
 import * as MU from '@mui/material';
+import Link from 'next/link';
 
 interface Props {
     product: IProduct
 }
 
 export const ProductCard: FC<Props> = ({ product }) => {
+
+    const [isHovered, setIsHovered] = useState(false)
+
+    const handleMouseEnter = () => {
+        setIsHovered(true)
+    }
+
+    const handleMouseLeave = () => {
+        setIsHovered(false)
+    }
+
+    const productImage = useMemo(() => {
+
+        return !isHovered
+            ? `products/${product.images[0]}`
+            : `products/${product.images[1]}`
+
+    }, [isHovered, product.images])
+
     return (
         <MU.Grid
             item
-            key={product.slug}
             xs={6}
             sm={4}
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}
         >
             <MU.Card>
-                <MU.CardActionArea>
-                    <MU.CardMedia
-                        component="img"
-                        image={`products/${product.images[0]}`}
-                        alt={product.title}
-                    />
-                </MU.CardActionArea>
+                <Link href={`/products/${product.slug}`} passHref prefetch={false}>
+                    <MU.CardActionArea>
+                        <MU.CardMedia
+                            className="fadeIn"
+                            component="img"
+                            image={productImage}
+                            alt={product.title}
+                        />
+                    </MU.CardActionArea>
+                </Link>
             </MU.Card>
+
+            <MU.Box sx={{ mt: 1 }} className="fadeIn">
+                <MU.Typography fontWeight={700}>{product.title}</MU.Typography>
+                <MU.Typography fontWeight={500}>{`$${product.price}`}</MU.Typography>
+            </MU.Box>
+
         </MU.Grid>
     )
 }
