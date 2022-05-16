9bcf42e324194124a739186e1088d32867780311
Jose Angarita <amjose09@gmail.com>
2022-05-15 19:33:51 -0500

    cookies master terminado

--- 04-cookies-master/pages/_app.tsx
+++ 04-cookies-master/pages/_app.tsx
@@ -1,16 +1,52 @@
 import '../src/styles/globals.css';
-import type { AppProps } from 'next/app'
+import type { AppContext, AppProps } from 'next/app'
 import { ThemeProvider } from '@emotion/react';
-import { darkTheme } from '../src/styles/themes';
-import { CssBaseline } from '@mui/material';
+import { customTheme, darkTheme, lightTheme } from '../src/styles/themes';
+import { CssBaseline, Theme } from '@mui/material';
+import Cookies from 'js-cookie';
+import { useEffect, useState } from 'react';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <ThemeProvider theme={darkTheme}>
-      <CssBaseline/>
-      <Component {...pageProps} />
-    </ThemeProvider>
-  )
+interface ExtendedApp extends AppProps {
+    theme: string
 }
+
+function MyApp({ Component, pageProps, theme = "light" }: ExtendedApp) {
+
+    const [currentTheme, setCurrentTheme] = useState(lightTheme)
+
+    useEffect(() => {
+        const frontTheme = Cookies.get("theme") || "dark";
+
+        const selectedTheme: Theme = (frontTheme === "light")
+            ? lightTheme
+            : (frontTheme === "dark")
+                ? darkTheme
+                : customTheme
+        setCurrentTheme(selectedTheme)
+    }, [])
+
+
+    return (
+        <ThemeProvider theme={currentTheme}>
+            <CssBaseline />
+            <Component {...pageProps} />
+        </ThemeProvider>
+    )
+}
+
+// MyApp.getInitialProps = async ({ ctx }: AppContext) => {
+
+//     // const { theme = "light" } = (ctx.req as any).cookies || {}
+
+//     //@ts-ignore
+//     const { theme = "light" } = (ctx.req?.cookies) || {}
+
+//     const validtThemes = ["light", "dark", "custom"]
+
+//     return {
+//         theme: validtThemes.includes(theme) ? theme : "dark"
+//     }
+
+// }
 
 export default MyApp
--- 04-cookies-master/pages/theme-changer/index.tsx
+++ 04-cookies-master/pages/theme-changer/index.tsx
@@ -1,20 +1,27 @@
-import { Card, CardContent, FormControl, FormControlLabel, FormLabel, Radio, RadioGroup } from '@mui/material'
+import { Button, Card, CardContent, FormControl, FormControlLabel, FormLabel, Radio, RadioGroup } from '@mui/material'
 import React, { useState } from 'react'
 import { Layout } from '../../src/components/layout'
 import Cookies from 'js-cookie'
-import { GetServerSideProps } from 'next'
+import { GetServerSideProps, NextPage } from 'next'
+import axios from 'axios'
 
 interface Props {
-
+    theme: string
 }
 
-const ThemeChangerPage = () => {
+const ThemeChangerPage: NextPage<Props> = ({ theme }) => {
 
-    const [currentTheme, setCurrentTheme] = useState("light")
-
+    const [currentTheme, setCurrentTheme] = useState(theme)
     const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         setCurrentTheme(event.target.value)
         Cookies.set("theme", event.target.value)
+    }
+
+    const onClick = async () => {
+
+        const { data } = await axios.get('/api/hello')
+        console.log(data)
+
     }
 
     return (
@@ -32,6 +39,11 @@ const ThemeChangerPage = () => {
                             <FormControlLabel value="custom" control={<Radio />} label="Custom" />
                         </RadioGroup>
                     </FormControl>
+
+                    <Button onClick={onClick}>
+                        Save
+                    </Button>
+
                 </CardContent>
             </Card>
         </Layout>
@@ -41,13 +53,13 @@ const ThemeChangerPage = () => {
 
 export const getServerSideProps: GetServerSideProps = async ({ req }) => {
 
-    const { theme = "light" } = req.cookies
+    const { theme } = req.cookies
 
-    console.log(theme)
+    const validThemes = ["light", "dark", "custom"]
 
     return {
         props: {
-
+            theme: validThemes.includes(theme) ? theme : "dark"
         }
     }
 }
